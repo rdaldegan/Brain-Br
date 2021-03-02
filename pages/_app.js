b4a73df73ef62291a100b7ccb2e02b8fa428ca01
Rafael Barreto Daldegan <ra.fa.daldegan@hotmail.com>
2021-03-02 10:19:55 -0300

    Primeiro commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Head from 'next/head';
+
+import Header from '../src/components/Header';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -6,21 +10,49 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
-`
+`;
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: '#0c0852',
+    secondary: '#12c0b4',
+    mainText: '#FFFFFF',
+    contrastText: '#12c0b4',
+    headerFooterBg: '#0c0852',
+    bodyBg: '#FFFFFF',
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Brain-Br – Neurociência Psicologia Urbanismo</title>
+        <link rel="icon" href="/round-icon.png" />
+        <meta name="title" content="Brain-Br" />
+        <meta name="description" content="Neurociência Psicologia Urbanismo" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://senhora-baforada-git-main.rdaldegan.vercel.app/" />
+        <meta property="og:title" content="Brain-Br" />
+        <meta property="og:description" content="Neurociência Psicologia Urbanismo" />
+        <meta property="og:image" content="/logo-black.jpg" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://senhora-baforada-git-main.rdaldegan.vercel.app/" />
+        <meta property="twitter:title" content="Brain-Br" />
+        <meta property="twitter:description" content="Neurociência Psicologia Urbanismo" />
+        <meta property="twitter:image" content="/logo-black.jpg" />
+
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet" />
+
+      </Head>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <Header />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components';
+import Image from 'next/image'
+
+const Container = styled.div`
+  flex: 1;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.headerFooterBg};
+`;
+
+const Head = styled.header`
+  width: 60%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Menu = styled.ul`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  list-style: none;
+
+  li{
+    color: ${({ theme }) => theme.colors.mainText};
+    font-size: 25px;
+    font-weight: 500;
+    margin-right: 25px;
+    
+    cursor: pointer;
+  }
+`;
+
+export default function Header() {
+  return (
+    <Container>
+      <Head>
+        <Image src="/Marca-completa.png" alt="Logo Brain Br" width={202} height={97} />
+        <Menu>
+          <li>
+            Sobre nós
+          </li>
+          <li>
+            Idealizadoras
+          </li>
+          <li>
+            Temas
+          </li>
+          <li>
+            Curso Destaque
+          </li>
+          <li>
+            Outros Cursos
+          </li>
+          <li>
+            Minha Conta
+          </li>
+        </Menu>
+      </Head>
+    </Container>
+  );
+}
