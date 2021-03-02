b6aa9f6331c5c89c9d712141ed2101002100de6f
Rafael Barreto Daldegan <ra.fa.daldegan@hotmail.com>
2021-03-02 11:19:55 -0300

    Ajuste nos tamanhos do header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -34,7 +34,7 @@ const Menu = styled.ul`
     color: ${({ theme }) => theme.colors.mainText};
     font-weight: 500;
     text-decoration: none;
-    margin-right: 15px;
+    margin-right: 7px;
     cursor: pointer;
     font-size: 20px;
     :hover {
@@ -55,7 +55,7 @@ const Menu = styled.ul`
     :last-child{
       background-color: ${({ theme }) => theme.colors.contrastText};
       color: ${({ theme }) => theme.colors.mainText};
-      font-size: 13px;
+      font-size: 5px;
       padding: 5px;
       :hover {
         transition: 250ms ease-in-out;
